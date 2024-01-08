ff5d644c872600a57763d9c5c03faaca0e192479
Maryam Tahhan <mtahhan@redhat.com>
2024-01-08 06:53:17 -0500

    enable dpdk 23.11 and af_xdp uds_path and pin_path
    Signed-off-by: Maryam Tahhan <mtahhan@redhat.com>

--- build.py
+++ build.py
@@ -344,7 +344,7 @@ def build_dpdk():
     if not os.path.exists('%s/build' % DPDK_DIR):
         configure_dpdk()
 
-    for f in glob.glob('%s/0001-net-af_xdp-enable-uds-path-instead-of-use_cni.patch' % DEPS_DIR):
+    for f in glob.glob('%s/afxdp_dp/*.patch' % DEPS_DIR):
         print('Applying patch %s' % f)
         cmd('patch -d %s -N -p1 < %s || true' % (DPDK_DIR, f), shell=True)
 
